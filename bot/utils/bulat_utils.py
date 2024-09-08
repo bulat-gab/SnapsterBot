27b509ea1caf3702d740601767414fe2fd081ca8
Bulat Gabdrakhmanov <gabdrakhmanov.br@gmail.com>
2024-09-08 15:18:02 +0700

    update bulat_utils

--- bot/utils/bulat_utils.py
+++ bot/utils/bulat_utils.py
@@ -1,3 +1,4 @@
+from random import random
 from better_proxy import Proxy
 from pyrogram import Client
 import json
@@ -5,6 +6,16 @@ import sys
 
 from . import logger
 
+def __get_proxies() -> dict[str, Proxy]:
+    proxies_json = None
+    with open(file="do_not_commit.proxies.json", encoding="utf-8-sig") as file:
+        proxies_json = json.load(file)
+
+    result = {}
+    for session_name, proxy_str in proxies_json.items():
+        result[session_name] = Proxy.from_str(proxy=proxy_str.strip())
+    
+    return result
 
 def get_proxies_with_clients(tg_clients: list[Client]) -> list[tuple[Client, Proxy]]:
     if not tg_clients:
@@ -14,9 +25,6 @@ def get_proxies_with_clients(tg_clients: list[Client]) -> list[tuple[Client, Pro
     proxies = __get_proxies()
     if not proxies:
         logger.error("Proxies not found.")
-        sys.exit()
-    if len(proxies) != len(tg_clients):
-        logger.error(f"Number of proxies and sessions do not match. Found '{len(proxies)}' proxies and '{len(tg_clients)}' sessions")
         sys.exit()
 
     result = []
@@ -33,13 +41,22 @@ def get_proxies_with_clients(tg_clients: list[Client]) -> list[tuple[Client, Pro
     logger.info(f"Found {len(result)} sessions with proxies.")
     return result
 
-def __get_proxies() -> dict[str, Proxy]:
-    proxies_json = None
-    with open(file="do_not_commit.proxies.json", encoding="utf-8-sig") as file:
-        proxies_json = json.load(file)
+def get_random_delay(left_hour: int = 1, right_hour: int = 10):
+    """
+        Get random delay between @left_hour and @right_hour that can be used with asyncio.sleep(delay)
+        returns delay in seconds
+    """
 
-    result = {}
-    for session_name, proxy_str in proxies_json.items():
-        result[session_name] = Proxy.from_str(proxy=proxy_str.strip())
+    if left_hour < 0:
+        left_hour = 1
+    if right_hour < 0:
+        right_hour = 10
+
+    delay = random.randint(8 * 3600, 11 * 3600)  # Random delay between 8 and 11 hours
+    return delay
+
+def get_hours_and_minutes(seconds: int) -> tuple[int, int]:
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
     
-    return result
+    return (hours, minutes)
